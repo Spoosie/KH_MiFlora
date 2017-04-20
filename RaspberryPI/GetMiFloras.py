ac9bd4d5b6c6b5c22104b7edf6765c589fcff841
Spoosie <kevinheidrich@me.com>
2017-04-20 20:01:21 +0200

    Raspi Script

---
+++ RaspberryPI/GetMiFloras.py
@@ -0,0 +1,37 @@
+import sys
+import time
+import datetime
+
+from gattlib import DiscoveryService
+from miflora.miflora_poller import MiFloraPoller, \
+    MI_CONDUCTIVITY, MI_MOISTURE, MI_LIGHT, MI_TEMPERATURE, MI_BATTERY
+
+def getPlant(macAdr):
+    poller = MiFloraPoller(macAdr)
+
+    now = time.time()
+    dtnow=datetime.datetime.fromtimestamp(now)
+    timeStr =  datetime.datetime.strftime(dtnow, '(%H:%M:%S %d-%m-%Y)')
+
+    fw = poller.firmware_version()
+    name = poller.name()
+    temp = poller.parameter_value("temperature")
+    moist = poller.parameter_value(MI_MOISTURE)
+    light = poller.parameter_value(MI_LIGHT)
+    cond = poller.parameter_value(MI_CONDUCTIVITY)
+    battery = poller.battery_level()
+
+    print(timeStr + " Mac="+macAdr+" Name="+name+" Fw="+fw+" Temp={:.2f} Moist={:d} Light={:d} Cond={:d} Bat={:d}".format(temp, moist, light, cond, battery))
+
+    return;
+
+
+
+service = DiscoveryService("hci0")
+devices = service.discover(2)
+
+for address, name in devices.items():
+    if name == "Flower care":
+        getPlant(address)
+
+print("DONE!")
