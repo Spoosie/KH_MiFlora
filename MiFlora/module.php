f6746c96405fe2dd169faeaf86cdac7be7f8e6e7
Spoosie <kevinheidrich@me.com>
2017-05-02 15:21:08 +0200

    New features
    * MIN/MAX Helligkeit und Temperatur
    * Kategorien umbenennen
    * Standort Medienanzeige

--- MiFlora/module.php
+++ MiFlora/module.php
@@ -17,7 +17,11 @@ class MiFlora extends IPSModule
         $this->RegisterPropertyBoolean("MinMaxEnabled", false);
         $this->RegisterPropertyBoolean("DataLoggingEnabled", false);
         $this->RegisterPropertyBoolean("BatteryCheckEnabled", false);
+        $this->RegisterPropertyBoolean("CreateReadableTextEnabled", false);
+        $this->RegisterPropertyString("FormSensorList", "");
+		
         $this->RegisterPropertyInteger("Debug", 0);
+		$this->CreateVariable("SensorList", 3,"{}", "miFloraSensorList", $this->InstanceID );
 		
 		// --------------------------------------------------------
         // Timer installieren
@@ -39,14 +43,22 @@ class MiFlora extends IPSModule
         {
             IPS_CreateVariableProfile("MiFlora_EC", 2);
             IPS_SetVariableProfileText("MiFlora_EC", "", " µs/cm");
-			IPS_SetVariableProfileValues("MiFlora_EC",0, 1000, 10 );
+			IPS_SetVariableProfileValues("MiFlora_EC",0, 1000, 100 );
         }
 		
         if (!IPS_VariableProfileExists("MiFlora_Humidity"))
         {
             IPS_CreateVariableProfile("MiFlora_Humidity", 2);
-			IPS_SetVariableProfileValues("MiFlora_Humidity",0, 100, 1 );
+			IPS_SetVariableProfileValues("MiFlora_Humidity",0, 100, 10 );
 			IPS_SetVariableProfileAssociation("MiFlora_Humidity", 0, "%d %%", "", -1);
+        }
+
+        if (!IPS_VariableProfileExists("MiFlora_Battery"))
+        {
+            IPS_CreateVariableProfile("MiFlora_Battery", 2);
+			IPS_SetVariableProfileValues("MiFlora_Battery",0, 100, 10 );
+			IPS_SetVariableProfileAssociation("MiFlora_Battery", 0, "%d %%", "", 0x00ff00);
+			IPS_SetVariableProfileAssociation("MiFlora_Battery", 10, "%d %%", "", 0xff0000);
         }
 
         if (!IPS_VariableProfileExists("MiFlora_Status"))
@@ -63,13 +75,15 @@ class MiFlora extends IPSModule
 			IPS_SetVariableProfileAssociation("MiFlora_Status", 10, "Zu viel von allem", "", 0xff0000);
         }
 		
-
+		// Verzeichnis für Standortbilder
+		mkdir(IPS_GetKernelDir()."webfront".DIRECTORY_SEPARATOR."user".DIRECTORY_SEPARATOR."MiFlora".DIRECTORY_SEPARATOR);
     }
     
     public function ApplyChanges()
     {
         //Never delete this line!
         parent::ApplyChanges();		
+
 		// --------------------------------------------------------
         // Timer starten
         // --------------------------------------------------------
@@ -81,6 +95,8 @@ class MiFlora extends IPSModule
     {
 		$sensorArray = NULL;
 		$updateScriptID = @IPS_GetScriptIDByName("SetValue",$this->InstanceID);
+		$sensorListID = IPS_GetVariableIDByName("SensorList",$this->InstanceID);
+		$sensorList = json_decode(GetValue($sensorListID),true);
 		
         $this->logThis("Updating from miflora hubs",self::LOGLEVEL_INFO);
 
@@ -148,7 +164,7 @@ class MiFlora extends IPSModule
 				$sensorArray[$uuid]["SoilMoisture"] = $result[6];
 				$sensorArray[$uuid]["Lux"] = $result[7];
 				$sensorArray[$uuid]["SoilElectricalConductivity"] = $result[8];
-				$sensorArray[$uuid]["BatteryLevel"] = $result[9]/100;
+				$sensorArray[$uuid]["BatteryLevel"] = $result[9];
 				$sensorArray[$uuid]["Hubs"][] = $mifloraHub->name;
 			}
 		}
@@ -161,7 +177,9 @@ class MiFlora extends IPSModule
 			
 			foreach($sensorArray as $uuid => $sensor)
 			{
-				$catID = $this->CreateCategory($sensor["UUID"]." (Bitte umbenennen!)", $uuid , $this->InstanceID);
+//				$formSensorData = GetFormSensorDataForUUID($sensor["UUID"]);				
+
+				$catID = $this->CreateCategory($sensor["UUID"]." (In Instanz umbenennen!)", $uuid , $this->InstanceID);
 				$this->CreateVariable("Letzte Meldung", 3, $sensor["LastMessage"], $uuid."_lastMessage", $catID );
 				$this->CreateVariable("UUID", 3, $sensor["UUID"], $uuid."_uuid", $catID );
 				$this->CreateVariable("Firmware", 3, $sensor["Firmware"], $uuid."_firmware", $catID);
@@ -170,8 +188,12 @@ class MiFlora extends IPSModule
 // Default Aktion sollte hier noch eingetragen werden			
 				$logVarIDs[] = $this->CreateVariable("Beleuchtungsstärke", 2, $sensor["Lux"], $uuid."_lux", $catID ,"MiFlora_LUX" );
 				$logVarIDs[] = $this->CreateVariable("Bodenleitfähigkeit", 2, $sensor["SoilElectricalConductivity"], $uuid."_soilElectricalConductivity", $catID ,"MiFlora_EC");
-				$logVarIDs[] = $this->CreateVariable("Zustand Batterie", 2, $sensor["BatteryLevel"], $uuid."_batteryLevel", $catID, "~Intensity.1" );
-				$this->CreateVariable("Hubs", 3, join($sensor["Hubs"],", "), $uuid."_hubs", $catID );		
+				$logVarIDs[] = $this->CreateVariable("Zustand Batterie", 2, $sensor["BatteryLevel"], $uuid."_batteryLevel", $catID, "MiFlora_Battery" );
+				$this->CreateVariable("Hubs", 3, join($sensor["Hubs"],", "), $uuid."_hubs", $catID );
+
+				$catObj = IPS_GetObject($catID);				
+				$sensorList[$sensor["UUID"]]["name"] = $catObj["ObjectName"];
+				$sensorList[$sensor["UUID"]]["uuid"] = $sensor["UUID"];
 
 				// Messwerte prüfen
 				if ($this->ReadPropertyBoolean("MinMaxEnabled"))
@@ -201,40 +223,111 @@ class MiFlora extends IPSModule
 						IPS_SetVariableCustomAction($varID, $updateScriptID);
 						$logVarIDs[] = $varID;
 					}
+					if (@IPS_GetVariableIDByName("Temperatur MAX", $catID) === false)
+					{
+						$varID = $this->CreateVariable("Temperatur MAX", 2, 35, $uuid."_tempMax", $catID ,"~Temperature.Room");
+						IPS_SetVariableCustomAction($varID, $updateScriptID);
+						$logVarIDs[] = $varID;
+					}
+					if (@IPS_GetVariableIDByName("Temperatur MIN", $catID) === false)
+					{
+						$varID = $this->CreateVariable("Temperatur MIN", 2, 10, $uuid."_tempMin", $catID ,"~Temperature.Room");
+						IPS_SetVariableCustomAction($varID, $updateScriptID);
+						$logVarIDs[] = $varID;
+					}
+					if (@IPS_GetVariableIDByName("Beleuchtungsstärke MAX", $catID) === false)
+					{
+						$varID = $this->CreateVariable("Beleuchtungsstärke MAX", 2, 15000, $uuid."_lightMax", $catID ,"MiFlora_LUX");
+						IPS_SetVariableCustomAction($varID, $updateScriptID);
+						$logVarIDs[] = $varID;
+					}
+					if (@IPS_GetVariableIDByName("Beleuchtungsstärke MIN", $catID) === false)
+					{
+						$varID = $this->CreateVariable("Beleuchtungsstärke MIN", 2, 0, $uuid."_lightMin", $catID ,"MiFlora_LUX");
+						IPS_SetVariableCustomAction($varID, $updateScriptID);
+						$logVarIDs[] = $varID;
+					}
 					
 
 					$hum = false;
 					$ec = false;
+					$temp = false;
+					$light = false;
 
 					// MIN/MAX Values laden - Könnten vom User verändert worden sein
 					$minHumID = IPS_GetVariableIDByName("Bodenfeuchtigkeit MIN", $catID);
 					$maxHumID = IPS_GetVariableIDByName("Bodenfeuchtigkeit MAX", $catID);
 					$minECID = IPS_GetVariableIDByName("Bodenleitfähigkeit MIN", $catID);
 					$maxECID = IPS_GetVariableIDByName("Bodenleitfähigkeit MAX", $catID);
+					$minTempID = IPS_GetVariableIDByName("Temperatur MIN", $catID);
+					$maxTempID = IPS_GetVariableIDByName("Temperatur MAX", $catID);
+					$minLightID = IPS_GetVariableIDByName("Beleuchtungsstärke MIN", $catID);
+					$maxLightID = IPS_GetVariableIDByName("Beleuchtungsstärke MAX", $catID);
 
 					$minHumidityValue = GetValue($minHumID);
 					$maxHumidityValue = GetValue($maxHumID);
 					$minECValue = GetValue($minECID);
 					$maxECValue = GetValue($maxECID);
+					$minTempValue = GetValue($minTempID);
+					$maxTempValue = GetValue($maxTempID);
+					$minLightValue = GetValue($minLightID);
+					$maxLightValue = GetValue($maxLightID);
 
 					$this->logThis("Hum=".$sensor["SoilMoisture"]. " Min=".$minHumidityValue." Max=".$maxHumidityValue,self::LOGLEVEL_DEBUG);
 					$this->logThis("Con=".$sensor["SoilElectricalConductivity"]. " Min=".$minECValue." Max=".$maxECValue,self::LOGLEVEL_DEBUG);
+					$this->logThis("Temp=".$sensor["Temperature"]. " Min=".$minTempValue." Max=".$maxTempValue,self::LOGLEVEL_DEBUG);
+					$this->logThis("Light=".$sensor["Lux"]. " Min=".$minLightValue." Max=".$maxLightValue,self::LOGLEVEL_DEBUG);
 					
 					$infoValue = 0;
 					// Feuchtigkeit
 					if ($sensor["SoilMoisture"] < $minHumidityValue)
+					{
 						$infoValue |= 1;
+						$readableText["waterPlease"][] = $catObj["ObjectName"];
+					}
 					else if ($sensor["SoilMoisture"] > $maxHumidityValue)
+					{
 						$infoValue |= 2;
+						$readableText["dryPlease"][] = $catObj["ObjectName"];
+					}
 
 					// Düngen
 					if ($sensor["SoilElectricalConductivity"] < $minECValue)
+					{
 						$infoValue |= 4;
+						$readableText["fertilizePlease"][] = $catObj["ObjectName"];
+					}
 					else if ($sensor["SoilElectricalConductivity"] > $maxECValue)
+					{
 						$infoValue |= 8;
+						$readableText["tooMuchFertilizer"][] = $catObj["ObjectName"];
+					}
 
-//					$this->logThis("Pflegehinweis=".$infoValue,self::LOGLEVEL_DEBUG);
+					// Temperatur
+					if ($sensor["Temperature"] < $minTempValue)
+					{
+						$infoValue |= 16;
+						$readableText["tooHot"][] = $catObj["ObjectName"];
+					}
+					else if ($sensor["Temperature"] > $maxHumidityValue)
+					{
+						$infoValue |= 32;
+						$readableText["tooCold"][] = $catObj["ObjectName"];
+					}
 
+					// Helligkeit
+					if ($sensor["Lux"] < $minECValue)
+					{
+						$infoValue |= 64;
+						$readableText["tooDark"][] = $catObj["ObjectName"];
+					}
+					else if ($sensor["Lux"] > $maxECValue)
+					{
+						$infoValue |= 128;
+						$readableText["tooBright"][] = $catObj["ObjectName"];
+					}
+
+					
 					$this->CreateVariable("Pflegehinweis", 1, $infoValue, $uuid."_infoText", $catID ,"MiFlora_Status");
 
 				}
@@ -260,6 +353,49 @@ class MiFlora extends IPSModule
 				}
 				
 			}
+
+			if ($this->ReadPropertyBoolean("CreateReadableTextEnabled"))
+			{
+				if (isset($readableText["waterPlease"]))
+					$readableText_waterPlease = "Folgende Pflanzen benötigen heute noch Wasser: ".join($readableText["waterPlease"],",");
+				else
+					$readableText_waterPlease = "Keine Pflanze benötigt Wasser.";
+				
+				if (isset($readableText["dryPlease"]))			
+					$readableText_dryPlease = "Folgende Pflanzen sind viel zu nass: ".join($readableText["dryPlease"],",");
+				else
+					$readableText_dryPlease = "Keine Pflanze ist zu nass.";
+					
+				if (isset($readableText["fertilizePlease"]))			
+					$readableText_fertilizePlease = "Folgende Pflanzen benötigen heute noch Dünger: ".join($readableText["fertilizePlease"],",");
+				else
+					$readableText_fertilizePlease = "Keine Pflanze benötigt dünger.";
+
+/*				
+				if (isset($readableText["tooMuchFertilizer"]))			
+					$readableText_tooMuchFertilizer = "Folgende Pflanzen sind Überdüngt worden: ".join($readableText["tooMuchFertilizer"],",");
+				else
+					$readableText_tooMuchFertilizer = "Keine Pflanze ist Überdüngt worden.";
+						
+				if (isset($readableText["tooBright"]))			
+					$readableText_tooBright = "Folgende Pflanzen stehen zu hell: ".join($readableText["tooBright"],",");
+				else
+					$readableText_tooBright = "Keine Pflanze steht zu hell.";
+*/
+						
+				$this->CreateVariable("Pflegehinweis (Giessen)", 3, $readableText_waterPlease, "miFlora_readableTextWaterPlease", $this->InstanceID);
+				$this->CreateVariable("Pflegehinweis (Trocknen)", 3, $readableText_dryPlease, "miFlora_readableTextDryPlease", $this->InstanceID);
+				$this->CreateVariable("Pflegehinweis (Düngen)", 3, $readableText_fertilizePlease, "miFlora_readableTextFertilizePlease", $this->InstanceID);
+/*
+				$this->CreateVariable("Pflegehinweis (Überdüngt)", 3, $readableText_fertilizePlease, "miFlora_readableTextFertilizePlease", $this->InstanceID);
+				$this->CreateVariable("Pflegehinweis (Zu dunkel)", 3, $readableText_fertilizePlease, "miFlora_readableTextFertilizePlease", $this->InstanceID);
+				$this->CreateVariable("Pflegehinweis (Zu hell)", 3, $readableText_fertilizePlease, "miFlora_readableTextFertilizePlease", $this->InstanceID);
+				$this->CreateVariable("Pflegehinweis (Zu kalt)", 3, $readableText_fertilizePlease, "miFlora_readableTextFertilizePlease", $this->InstanceID);
+				$this->CreateVariable("Pflegehinweis (Zu warm)", 3, $readableText_fertilizePlease, "miFlora_readableTextFertilizePlease", $this->InstanceID);
+*/
+				}
+			
+			SetValue($sensorListID,json_encode($sensorList));
 		}
 		
 	}
@@ -362,7 +498,7 @@ class MiFlora extends IPSModule
 
     }
 
-	private function logThis($message,$logLevel)
+	private function logThis($message,$logLevel,$logToDisc = false)
 	{
 		
 		if ($this->ReadPropertyInteger("Debug") < $logLevel)
@@ -381,19 +517,89 @@ class MiFlora extends IPSModule
 				break;
 		}
 
+		if ($logToDisc)
+		{
+			$log = @file_get_contents(__DIR__ . "/log.txt");			
+			file_put_contents(__DIR__ . "/log.txt",$log.$message."\n"); 		
+		}
+
 	}
+
+	private function RenameCategory($Ident,$Name)
+	{
+		$CatID = @IPS_GetObjectIDByIdent( $Ident, $this->InstanceID );
+
+		$this->logThis("RenameCategory (Ident:".$Ident.",Name:".$Name.",CatID:".$CatID.")",self::LOGLEVEL_INFO);
+		
+		if ($CatID !== false)
+			IPS_SetName( $CatID, $Name );
+
+		return $CatID;
+	}
+
+	private function GetFormSensorDataForUUID($uuid)
+	{
+		$formSensorList = $this->ReadPropertyString("FormSensorList");
+		$formSensorList = json_decode($formSensorList,true);
+		
+//		$this->logThis("FormSensorList=".print_r($formSensorList,true),self::LOGLEVEL_INFO);
+		
+		foreach($formSensorList as $entry)
+		{
+			if ($entry["uuid"] == $uuid)
+				return $entry;
+		}
+		
+		return false;
+	}
+	
+	
 	
 	public function GetConfigurationForm()
 	{		
+		$this->logThis("GetConfigurationForm",self::LOGLEVEL_INFO);
 		
-//		$data = json_decode(file_get_contents(__DIR__ . "/form.json")); 	
 		
-//	$this->logThis(print_r($data),self::LOGLEVEL_INFO);
+		$data = json_decode(file_get_contents(__DIR__ . "/form.json")); 		
+
 		
-//		$data->actions[0]->columns[] = Array("name" => "UUID", "label" => "UUID", "width" => "100px");
-//		$data->actions[0]->values[] = Array("name" => "UUID", "label" => "123");
-							
-//		return json_encode($data);
+		// Aktuell gefundene Sensoren. 
+		if ($sensorListID = @IPS_GetVariableIDByName("SensorList",$this->InstanceID))
+			$sensorList = json_decode(GetValue($sensorListID),true);
+
+		
+//		$this->logThis("Aktuell gefundene Sensoren=".sizeof($sensorList),self::LOGLEVEL_INFO);
+		
+		foreach($sensorList as $sensor)
+		{			
+//			$this->logThis(print_r($sensor,true),self::LOGLEVEL_INFO);
+			$uuid = str_replace(":","",$sensor["uuid"]);
+
+
+			$formSensor = $this->GetFormSensorDataForUUID($sensor["uuid"]);
+			
+			if ($formSensor != false)
+			{
+				// Kategorie umbennen
+				$catID = $this->RenameCategory($uuid,$formSensor["name"]);
+				// Infobild 				
+				if (isset($formSensor["mediaID"]) && $formSensor["mediaID"] > 0)
+				{
+					$imgContent = base64_decode(IPS_GetMediaContent($formSensor["mediaID"]));
+					$imgPath = IPS_GetKernelDir()."webfront".DIRECTORY_SEPARATOR."user".DIRECTORY_SEPARATOR."MiFlora".DIRECTORY_SEPARATOR.$uuid.".jpg";
+					file_put_contents($imgPath, $imgContent);
+
+					$this->CreateVariable("Standort", 3, "<center><img style='width:100%;' src='/user/MiFlora/".$uuid.".jpg'></center>", $uuid."_place", $catID , "~HTMLBox");
+				}
+			}
+			$data->elements[1]->values[] = $sensor;
+		}
+
+		$content = json_encode($data);
+
+//		$this->logThis(print_r($content,true),self::LOGLEVEL_INFO);
+		
+		return $content;
 	
 	} 
 }
