ad94c78236641f3102110582c2df40f017fd49b5
micbuh <micbuh@gmail.com>
2017-10-29 11:40:08 +0000

    fixed script to work with latest changes at https://github.com/open-homeautomation/miflora (changed api).

--- docs/GetMiFloras.py
+++ docs/GetMiFloras.py
@@ -5,9 +5,10 @@ import datetime
 from gattlib import DiscoveryService
 from miflora.miflora_poller import MiFloraPoller, \
     MI_CONDUCTIVITY, MI_MOISTURE, MI_LIGHT, MI_TEMPERATURE, MI_BATTERY
+from miflora.backends.gatttool import GatttoolBackend
 
 def getPlant(macAdr):
-    poller = MiFloraPoller(macAdr)
+    poller = MiFloraPoller(macAdr, GatttoolBackend)
 
     now = time.time()
     dtnow=datetime.datetime.fromtimestamp(now)
@@ -21,7 +22,7 @@ def getPlant(macAdr):
     cond = poller.parameter_value(MI_CONDUCTIVITY)
     battery = poller.battery_level()
 
-    print(timeStr + " Mac="+macAdr+" Name="+name+" Fw="+fw+" Temp={:.2f} Moist={:d} Light={:d} Cond={:d} Bat={:d}".format(temp, moist, light, cond, battery))
+    print(timeStr + " Mac="+macAdr+" Name="+name+" Fw="+fw+" Temp={:.2f} Moist={:d} Light={:d} Cond={:d} Bat={:d}".forma$
 
     return;
 
@@ -35,3 +36,4 @@ for address, name in devices.items():
         getPlant(address)
 
 print("DONE!")
+
